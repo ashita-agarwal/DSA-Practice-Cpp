473fb64e9de63e8f05ac4bef223adf17e2c4ff10
Ashita Agarwal <63288542+ashita-agarwal@users.noreply.github.com>
2021-02-06 01:22:12 +0530

    Add files via upload
               1
             1   1
           1   2   1
         1   3   3   1
    
    for i/p=4

---
+++ Basics/pascalTriangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int N,row,col,space;
+	cin>>N;//number of rows
+	for (row=0;row<N;row++){
+		
+		int val=1;//inside for loop
+		for(space=0;space<=(N-row-1);space++){ //spaces
+				cout<<" ";
+		}
+		for(col=0;col<=row;col++){ //column
+			cout<<val<<" "; //value to be printed
+			val=val*(row-col)/(col+1); //the formula 
+		}
+	cout<<endl;
+	}
+	return 0;
+}
