285fdce2aaaee530ad4a9bd4f813566d8a631c67
Ashita Agarwal <63288542+ashita-agarwal@users.noreply.github.com>
2021-02-10 00:06:24 +0530

    Add files via upload
    Sample Input
    5
    1
    3
    4
    2
    5
    5
    Sample Output
    1 and 4
    2 and 3

---
+++ Arrays/Target Sum Pairs.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int n,i;
+    cin>>n;
+    int arr[n]={};
+
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    //insertion sort
+    for (int i=1;i<=n;i++){
+       int hand=arr[i]; //take one vale  from unsorted value  in hand 
+       int j;
+        for( j=i-1;j>=0 and arr[j]>hand;j--){ 
+
+            
+               arr[j+1]=arr[j];
+                               
+            
+            
+        } arr[j+1]=hand;
+         
+    }
+    
+    // for (int i=0;i<n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    int left=0, right=n;
+    while(left<right){
+        if(arr[left]+arr[right]>target){
+            right--;
+        }else if(arr[left]+arr[right]<target){
+            left++;
+        }else{
+            cout<<arr[left]<<" and "<<arr[right];
+            
+            left++;
+            cout<<endl;
+        }
+    }
+    return 0;
+}
