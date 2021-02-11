416b39eef96f669f390de769695c52410d745490
Ashita Agarwal <63288542+ashita-agarwal@users.noreply.github.com>
2021-02-11 19:51:46 +0530

    Add files via upload

---
+++ IMP QUES/max Subarray Sum (kadane's algo).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //kadane's algo
+    int cs=0,ms=0; //currentSum, MAxSum
+    int t; //testcases
+    cin>>t;
+    while(t--){
+        int cs=0,ms=0,n,arr[100005]; //ms can also be INT_MIN
+        cin>>n ; 
+        //number of element in array
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            cs=cs+arr[i]; //add elem to current sum
+            if(cs<0){
+                cs=0;
+            }
+            ms=max(ms,cs); 
+        }
+        cout<<ms<<endl; //print max Sum
+    }
+    return 0;
+}
