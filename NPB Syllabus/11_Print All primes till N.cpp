8249cfc5e2e2dec1267caddb6d9fa15e4710f985
Ashita Agarwal <63288542+ashita-agarwal@users.noreply.github.com>
2021-03-05 18:53:02 +0530

    Update 11_Print All primes till N.cpp

--- NPB Syllabus/11_Print All primes till N.cpp
+++ NPB Syllabus/11_Print All primes till N.cpp
@@ -1,12 +1,27 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n; //print all prime till n
+    int n;
     cin>>n;
-    for(int i=2;i<n;i++){
-        if(n%2!=0){
-            cout<<i<<endl;
+    bool isPrime;
+    for(int i=1;i<=n;i++){ //every number will b checked till N
+            if(i==0 || i==1){
+                continue;
+            }
+            isPrime=true;
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0){
+                    isPrime=false;
+                    break;
+                }
+                
+            }
+           if(isPrime==true){
+            cout<<i<<" ";
         }
+        
+         
+        
     }
 return 0;
 }
