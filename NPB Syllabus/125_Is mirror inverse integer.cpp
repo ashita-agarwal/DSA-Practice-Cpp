5bcc08d0fdb984ff96ee53008c2f6600840ab581
Ashita Agarwal <63288542+ashita-agarwal@users.noreply.github.com>
2021-03-09 22:03:53 +0530

    Add files via upload

---
+++ NPB Syllabus/125_Is mirror inverse integer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int a[10000]={0};//array where number will b stored
+    int i=1;
+    while(n!=0){
+        a[i]=n%10;
+        n=n/10;
+        i++;
+    }
+    int flag=1;
+    for(int j=0;j<i-1;j++){
+        if(a[a[j]]==j){
+            continue;
+
+        }else{
+            flag =0;
+            break;
+        }
+    }
+    if(flag==1){
+        cout<<"true";
+    }else{
+        cout<<"false";
+    }
+    return 0;
+}
