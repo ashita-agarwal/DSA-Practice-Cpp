08ae0fc11b38d08c61d306aa6a466de10a306955
Ashita Agarwal <63288542+ashita-agarwal@users.noreply.github.com>
2021-03-15 21:23:35 +0530

    Add files via upload
    ccodingbllocksAaBbCDEF
    
    locksAaBbCDEF(13)

---
+++ strings/longest substring without repeating char.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+bool isValid(int *freq){
+    for(int i=0;i<256;i++){
+        if(freq[i]>1){
+            return false;
+        }
+    }
+    return true;
+}
+int solve(char* a){
+    int i=0,j=0;
+    int freq[256]={0};
+    int ans=0;
+    while(a[j]!='\0'){
+        freq[a[j]]++;
+        
+        if(isValid(freq)){
+            ans = max(ans,j-i+1);
+        }else{
+            while(isValid(freq)!=true){
+                freq[a[i++]]--;
+            }
+        }
+        j++;
+    }
+    return ans;
+}
+
+int main() {
+	// your code goes here
+	char a[100];
+	cin>>a;
+	cout<<solve(a)<<endl;
+	return 0;
+}
