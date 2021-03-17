dfdcb3e93c0df04870315fcf676afd13ba768a2e
Ashita Agarwal <63288542+ashita-agarwal@users.noreply.github.com>
2021-03-17 23:43:06 +0530

    Add files via upload

---
+++ strings/reverse letters of words in a string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string s;
+	getline(cin,s);
+	
+	for(int i=0;i!=s.length();i++){
+	    string empty_string="";
+	    while(s[i]!=' ' && i<s.length()){
+	        empty_string.push_back(s[i]);
+	        i++;
+	    }
+	    reverse(empty_string.begin(),empty_string.end());
+	    cout<<empty_string<<" ";
+	}
+	
+	return 0;
+}
