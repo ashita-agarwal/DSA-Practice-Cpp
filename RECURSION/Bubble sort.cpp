e729316af0c6537e39da26aa75264f3567e6b89e
Ashita Agarwal <63288542+ashita-agarwal@users.noreply.github.com>
2021-03-22 19:40:31 +0530

    Update Bubble sort.cpp

--- RECURSION/Bubble sort.cpp
+++ RECURSION/Bubble sort.cpp
@@ -5,13 +5,32 @@ void BubbleSort(int *a,int n,int i){
     if(i==n-1){
         return;
     }
-    //recursive case
+    
     for(int j=0;j<n-1;j++){
         if(a[j]>a[j+1]){
             swap(a[j],a[j+1]);
         }
     }
     BubbleSort(a,n,i+1);
+    
+}
+void BubbleSortRec(int *a,int n,int i,int j){
+    //base case
+    if(i==n-1){
+        return;
+    }
+    if(j==n-1){
+        BubbleSortRec(a,n,i+1,0);
+        
+    }
+    else{
+        if(a[j]>a[j+1]){
+            swap(a[j],a[j+1]);
+        }
+        BubbleSortRec(a,n,i,j+1);
+    }
+    
+    
 }
 void print(int *a,int n){
     for(int i=0;i<n;i++){
@@ -24,6 +43,9 @@ int main() {
     int n=5;
    
     BubbleSort(a,n,0);
+
+     print(a,n);
+     BubbleSortRec(a,n,0,0);
      print(a,n);
     return 0;
 }
